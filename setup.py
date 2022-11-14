bd23b71fd558707beb50e7ba2580e925ce52be49
Mychael Gray <graymychael@gmail.com>
2022-11-13 21:48:40 -0800

    Updated all files and directories

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The purpose of this data science project is to come up with a pricing model for diamonds in the diamond market. Using hThis project aims to build a predictive model for diamond price based on a number of features, or properties industry standard diamonds. This model will be used to provide guidance for diamond pricing and future value investment plans.',
+    author='Mychael Gray',
+    license='MIT',
+)
